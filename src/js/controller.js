61f00bb49157908c00da441d67c0412025144a5d
ducla <ducla.bacha@gmail.com>
2023-06-30 10:22:12 +0700

    clear up

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,10 +119,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function {
-  console.log('Welcome to the application!');
-}
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
